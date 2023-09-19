7decdc1993cd8da24c6a033c82c9667a735b275b
anarsantos <aninha_rsantos@yahoo.com.br>
2023-09-19 15:39:11 -0300

    Orientação a objetos

--- main.dart
+++ main.dart
@@ -1,10 +1,41 @@
-import 'pessoa.dart';
+import 'cartao.dart';
+import 'cliente.dart';
+import 'compra.dart';
+import 'conta.dart';
+import 'fatura.dart';
 
 main() {
-  Pessoa joao = Pessoa(
-    nome: 'João', 
-    sobrenome: 'Silva',
-    );
-
-  print('O completo é ${joao.nomeCompleto}.');  
+  var conta = Conta(
+    cliente: Cliente(
+      id: '12345', 
+      cpf: '012.345.678-90', 
+      nome: 'Jose', 
+      sobrenome: 'Silva',
+      ),
+    cartao: Cartao(
+      numero: '123456789', 
+      limite: 12500, 
+      mes: 06, 
+      ano: 2025, 
+      codigo: 123,
+      ), 
+    faturas: [
+      Fatura(
+        compras: [
+          Compra(
+            valor: 12.5, 
+            descricao: 'Cafe', 
+            data: '12/06',
+            ),
+          Compra(
+            valor: 100.0, 
+            descricao: 'Mercado', 
+            data: '14/06',
+            ),
+        ], 
+        mes: 6, 
+        ano: 2021,
+        ),
+    ],
+    );  
 }
